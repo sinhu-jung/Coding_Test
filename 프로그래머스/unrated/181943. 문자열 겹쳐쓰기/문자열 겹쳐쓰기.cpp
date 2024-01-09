f35bc0299ad58ca5d8a34244042a85325365494b
sinhu-jung <67888402+sinhu-jung@users.noreply.github.com>
2024-01-09 12:15:55 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, string overwrite_string, int s) {
+    string answer = my_string;
+    answer.replace(s, overwrite_string.size(), overwrite_string);
+    return answer;
+}

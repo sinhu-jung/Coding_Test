794e1b5a27730bf5c60adb7cba3e2ec743ab4db6
sinhu-jung <67888402+sinhu-jung@users.noreply.github.com>
2024-01-22 18:23:31 +0900

    [level 0] Title: 수 조작하기 1, Time: 0.17 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, string control) {
+    int answer = n;
+    for(int i = 0; i < control.size(); i++) {
+        if(control[i] == 'w') {
+            answer += 1;
+        }
+        else if (control[i] == 's') {
+            answer -= 1;
+        }
+        else if (control[i] == 'd') {
+            answer += 10;
+        }
+        else {
+            answer -= 10;
+        }
+    }
+    return answer;
+}

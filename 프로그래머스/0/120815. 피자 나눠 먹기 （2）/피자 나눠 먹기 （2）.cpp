e9a9cfa6338cba3dc59edd519bb4fdbe1346f1dd
sinhu-jung <67888402+sinhu-jung@users.noreply.github.com>
2024-01-25 10:01:35 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int pizza = 6;
+    while( pizza % n != 0) {
+        pizza += 6;
+    }
+    
+    return pizza / 6;
+}

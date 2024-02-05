50910ac6d8b6f4510e02dbeb8f9f37b812ecdec5
sinhu-jung <67888402+sinhu-jung@users.noreply.github.com>
2024-02-05 11:15:24 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    double a = 0, b = 0;
+    
+    cout.precision(12);
+    cin >> a >> b;
+    cout << a / b << endl;
+    
+    return 0;
+}

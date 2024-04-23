35844e2884102aa9e83e2766dde761cfa3244252
sinhu-jung <67888402+sinhu-jung@users.noreply.github.com>
2024-04-23 11:59:37 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(void){
+	cout << "|\\_/|\n";
+	cout << "|q p|   /}\n";
+	cout << "( 0 )\"\"\"\\\n";
+	cout << "|\"^\"`    |\n";
+	cout << "||_/=\\\\__|\n";
+	return 0;
+}

53cea22f13a0d23c1cb59e56b3ca7a2c757e565a
sinhu-jung <67888402+sinhu-jung@users.noreply.github.com>
2024-04-23 11:58:25 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(void){
+    cout << "\\    /\\\n";
+    cout << " )  ( ')\n";
+    cout << "(  /  )\n";
+    cout << " \\(__)|\n";
+	return 0;
+}

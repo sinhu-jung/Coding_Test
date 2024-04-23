3f175947927138dcb561ea1039f5d0632dbd2ffb
sinhu-jung <67888402+sinhu-jung@users.noreply.github.com>
2024-04-23 11:52:14 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(void){
+	long long a = 0, b = 0, c = 0;
+	cin >> a >> b >> c;
+	cout << a + b + c << endl;
+}

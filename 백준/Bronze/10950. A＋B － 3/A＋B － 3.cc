d7180482faf5f951d0c0108415d7b9d7286dd3a8
sinhu-jung <67888402+sinhu-jung@users.noreply.github.com>
2024-04-24 08:26:01 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void){
+	int a = 0;
+	cin >> a;
+	
+	for(int i = 0; i < a; i++) {
+		int b = 0, c = 0;
+		cin >> b >> c;
+		cout << b + c << endl;
+	}
+	
+	return 0;
+}

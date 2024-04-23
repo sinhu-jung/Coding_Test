7cb23d655d2ada4cad731b8061ede0333c7c5864
sinhu-jung <67888402+sinhu-jung@users.noreply.github.com>
2024-04-23 12:30:06 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(void){
+	int a = 0, b =0;
+	cin >> a >> b;
+	
+	if( a > 0 && b > 0) {
+		cout << 1 << endl;
+	}
+	else if ( a < 0 && b > 0) {
+		cout << 2 << endl;
+	}
+	else if ( a < 0 && b < 0) {
+		cout << 3 << endl;
+	}
+	else {
+		cout << 4 << endl;
+	}
+}

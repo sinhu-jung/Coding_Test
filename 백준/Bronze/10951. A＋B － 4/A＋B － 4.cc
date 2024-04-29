b806459d2cf9a99b4bd00803c56c4057e4230e6d
sinhu-jung <67888402+sinhu-jung@users.noreply.github.com>
2024-04-29 09:45:15 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int b = 0, c = 0;
+	
+	
+	while(!(cin >> b >> c).eof()) {
+		cout << b + c << "\n";
+	}
+
+	return 0;
+}

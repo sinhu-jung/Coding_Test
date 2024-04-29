1261f2c8a78b171bb8c35e57d9f164c2a878c00a
sinhu-jung <67888402+sinhu-jung@users.noreply.github.com>
2024-04-29 09:29:12 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int a = 0, b = 0, c = 0;
+	
+	cin >> a;
+	
+	for(int i = 0; i < a; i++) {
+		cin >> b >> c;
+		cout << "Case #" << i + 1  << ": " << b + c << "\n";
+	}
+
+	return 0;
+}
